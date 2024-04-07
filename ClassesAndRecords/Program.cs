94a6a4ec77cc95fe75b628ae4c8f6d337f364af6
AVANADE-CORP\ana.chavez.velasco <ana.chavez.velasco@gmail.com>
2024-04-08 00:11:30 +0200

    Añadido de Name y Author

--- ClassesAndRecords/Program.cs
+++ ClassesAndRecords/Program.cs
@@ -4,10 +4,27 @@ namespace ClassesAndRecords
 {
     class Program
     {
+        static void PrintCourse(Course course)
+        {
+
+        }
+        
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            var pluralsightCourse = new Course();
+            pluralsightCourse.Name = "Working with C# Records";
+            pluralsightCourse.Author = "Roland Guijt";
+
+            var anotherCourse = pluralsightCourse;
+            
+            Console.WriteLine(pluralsightCourse.Name);
+            Console.WriteLine(anotherCourse.Name);
+        } 
+    }
+    public class Course
+    {
+        public string Name { get; set; }
+        public String Author { get; set; } // Escribo "prop" y pulso tab para que me cree automáticamente una propiedad
     }
 }
 
